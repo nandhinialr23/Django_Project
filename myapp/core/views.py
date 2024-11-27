3cccd9aa26a6cd1e0541c9a4bdcddb467cf8faad
Nayeem Islam <nayeem60151126@gmail.com>
2024-11-27 11:31:28 +0600

    Updated View for CI?CD

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    context = {"message": "This is an updated view from the CD pipeline!"}
+    return render(request, "home.html", context)
