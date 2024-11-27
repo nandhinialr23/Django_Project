c1ac4f3531b05199874a7bbeda31463e4f69df4b
Nayeem Islam <nayeem60151126@gmail.com>
2024-11-27 11:28:37 +0600

    Update settings.py

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6w66jh%^ijz)c+8j45e#kw3ml$616jtsb2mfaraxum(=hg4_7o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
